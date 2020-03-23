e1734225694de3c7fcb538ae21eda6468d6d1d9c
Thomas Haslwanter <thomas.haslwanter@fh-linz.at>
2020-03-23 22:31:32 +0100

    new/renamed python programs

---
+++ Listings/L2_4_pythonModule.py
@@ -0,0 +1,44 @@
+""" Demonstration of a Python Function """
+
+# author:   Thomas Haslwanter
+# date:     March-2020
+
+# Import standard packages
+import numpy as np
+
+
+def income_and_expenses(data):
+    """Find the sum of the positive numbers, and the sum of the negative ones.
+
+    Parameters
+    ----------
+    data : numpy array (,n)
+           Incoming and outgoing account transactions
+
+    Returns
+    -------
+    income : float
+             Sum of the incoming transactions
+    expenses : float
+             Sum of the outgoing account transactions
+    """
+    
+    income = np.sum(data[data>0])
+    expenses = np.sum(data[data<0])
+    
+    return (income, expenses)
+
+
+if __name__=='__main__':
+    testData = np.array([-5, 12, 3, -6, -4, 8])
+    
+    # If only real banks would be so nice ;)
+    if testData[0] < 0:
+        print('Your first transaction was a loss, and will be dropped.')
+        testData = np.delete(testData, 0)
+    else:
+        print('Congratulations: Your first transaction was a gain!')
+        
+    (my_income, my_expenses) = income_and_expenses(testData)
+    print(f'You have earned {my_income:5.2f} EUR, and spent {-my_expenses:5.2f} EUR.')
+
