db7b3366ef20dd78efef9e4b019271c699379aef
Thomas Haslwanter <thomas.haslwanter@fh-linz.at>
2020-04-26 11:47:23 +0200

    Tests added

---
+++ Tests/test_Figures.py
@@ -0,0 +1,22 @@
+""" Test routine for source code that generates 'F'igures """
+
+# author:   Thomas Haslwanter
+# date:     April-2020
+
+# additional packages
+import os
+import sys
+
+figDir = os.path.join('..', r'SAP/Figures')
+os.chdir(figDir)
+startNr = 0     # if you don't want to go through all files
+
+# make sure local imports are working
+sys.path.append('.')
+
+pyList = [file for file in os.listdir('.') if (file[-3:]=='.py' and file[0]=='F')]
+for number, file in enumerate(pyList):
+    print(f'{number+1}/{len(pyList)}: {file}')
+    if number >= startNr:
+        exec(open(file).read())
+    
---
+++ Tests/test_Solutions.py
@@ -0,0 +1,24 @@
+""" Test exercise solutions for
+'Hands-on Introduction to Signalanalysis with Python'
+"""
+
+# author:   Thomas Haslwanter
+# date:     April-2020
+
+# additional packages
+import os
+import sys
+
+figDir = os.path.join('..', r'SAP/Exercise_Solutions')
+os.chdir(figDir)
+startNr = 0     # if you don't want to go through all files
+
+# make sure local imports are working
+sys.path.append('.')
+
+pyList = [file for file in os.listdir('.') if (file[-3:]=='.py' and file[0]=='S')]
+for number, file in enumerate(pyList):
+    print(f'{number+1}/{len(pyList)}: {file}')
+    if number >= startNr:
+        exec(open(file).read())
+    
