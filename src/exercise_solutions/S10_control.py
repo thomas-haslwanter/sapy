25f6049fe62589a007e874c09054808f8867ccb5
Thomas Haslwanter <thomas.haslwanter@fh-linz.at>
2020-06-14 23:51:20 +0200

    anscombe, binary npz-data added

--- src/exercise_solutions/S10_control.py
+++ src/exercise_solutions/S10_control.py
@@ -1,7 +1,7 @@
-""" Solution Exercise 12.x 'Simulation of Muscles' """
+""" Solution Exercise Chapter 10: Equations of Motion """
 
 # author:   Thomas Haslwanter
-# date:     April-2020
+# date:     June-2020
 
 # Import the required packages
 import numpy as np
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import control
 from collections import namedtuple
 
-# Define the paramters
+# Define the parameters
 r = 2
 k = 1
 
@@ -17,21 +17,30 @@ k = 1
 sys_spring = control.TransferFunction([k], [1])
 sys_damper = control.TransferFunction([r,0], [1])
 
-# Combine these for a Voigt-element, and for spring and damper in seial connection
+# Combine these appropriately
 voigt = 1/(control.parallel(sys_spring, sys_damper))
 serial = 1/(control.series(sys_spring, sys_damper))
+voigt_serial = control.series(voigt, voigt)
+
+# Display the transfer functions
+print(voigt)
+print(serial)
+print(voigt_serial)
 
 # Give the output simple names, and specify the time axis
 Response = namedtuple('Response', ['t', 'x'])
 t_out = np.arange(0,10,0.1)
 
-# Simulate a step response of these two systems
+# Simulate a step response of these three systems
 r_voigt = Response(*control.step_response(voigt, t_out))
 r_serial = Response(*control.step_response(serial, t_out))
+r_voigt_serial = Response(*control.step_response(voigt_serial, t_out))
 
 # Plot the solutions
 plt.plot(r_voigt.t, r_voigt.x, label='Voigt')
 plt.plot(r_serial.t, r_serial.x, label='Serial')
+plt.plot(r_voigt_serial.t, r_voigt_serial.x, label='Voigt-Serial')
+
 plt.legend()
 plt.xlabel('Time [sec]')
 plt.ylabel('System Responses')
---
+++ src/figures/F8_5_anscombe.py
@@ -0,0 +1,74 @@
+"""
+==================
+Anscombe's quartet
+==================
+
+`Anscombe's quartet`_ is a group of datasets (x, y) that have the same mean,
+standard deviation, and regression line, but which are qualitatively different.
+
+It is often used to illustrate the importance of looking at a set of data
+graphically and not only relying on basic statistic properties.
+
+.. _Anscombe's quartet: https://en.wikipedia.org/wiki/Anscombe%27s_quartet
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from utilities.my_style import show_data 
+
+x = [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5]
+y1 = [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
+y2 = [9.14, 8.14, 8.74, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]
+y3 = [7.46, 6.77, 12.74, 7.11, 7.81, 8.84, 6.08, 5.39, 8.15, 6.42, 5.73]
+x4 = [8, 8, 8, 8, 8, 8, 8, 19, 8, 8, 8]
+y4 = [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 12.50, 5.56, 7.91, 6.89]
+
+datasets = {
+    'I': (x, y1),
+    'II': (x, y2),
+    'III': (x, y3),
+    'IV': (x4, y4)
+}
+
+fig, axs = plt.subplots(2, 2, sharex=False, sharey=False, figsize=(8, 5),
+                        gridspec_kw={'wspace': 0.08, 'hspace': 0.08})
+
+ii = 0
+for ax, (label, (x, y)) in zip(axs.flat, datasets.items()):
+    ax.text(0.1, 0.9, label, fontsize=20, transform=ax.transAxes, va='top')
+    ax.set(xlim=(0, 20), ylim=(2, 14))
+    ax.set(xticks=(0, 10, 20), yticks=(4, 8, 12))
+    ax.tick_params(direction='in', top=True, right=True)
+    
+    ax.spines['right'].set_visible(False)
+    ax.yaxis.set_ticks_position('left')
+    
+    ax.spines['top'].set_visible(False)
+    ax.xaxis.set_ticks_position('bottom')
+    
+    cur_color = 'C' + str(ii)
+    ax.plot(x, y, 'o', color=cur_color, ms=4)
+    ii += 1
+
+    # linear regression
+    p1, p0 = np.polyfit(x, y, deg=1)
+    x_lin = np.array([np.min(x), np.max(x)])
+    y_lin = p1 * x_lin + p0
+    ax.plot(x_lin, y_lin, color=cur_color, lw=1)
+
+    # add text box for the statistics
+    stats = (f'$\\mu$ = {np.mean(y):.2f}\n'
+             f'$\\sigma$ = {np.std(y):.2f}\n'
+             f'$r$ = {np.corrcoef(x, y)[0][1]:.2f}')
+    bbox = dict(boxstyle='round', fc='blanchedalmond', ec='orange', alpha=0.5)
+    ax.text(0.95, 0.07, stats, fontsize=9, bbox=bbox,
+            transform=ax.transAxes, horizontalalignment='right')
+
+for ax in [axs[0,0], axs[0,1]]:
+    ax.set_xticklabels('')
+    
+for ax in [axs[0,1], axs[1,1]]:
+    ax.set_yticklabels('')
+    
+out_file = 'anscombe.jpg'    
+show_data(out_file)
