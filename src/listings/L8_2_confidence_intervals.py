e9b5da2c767b6ea7a903953ed7c93a1dca3ab4d4
Thomas Haslwanter <office@thaslwanter.at>
2026-08-24 15:08:08 +0200

    Code for 95%-CIs with pingouin added

--- src/listings/L8_2_confidence_intervals.py
+++ src/listings/L8_2_confidence_intervals.py
@@ -1,19 +1,28 @@
-""" Calculation of confidence intervals for a line-fit """
+"""Calculation of confidence intervals for a line-fit"""
 
 # Import the required packages
 import numpy as np
 import pandas as pd
 import statsmodels.formula.api as smf
-from pprint import pprint
+import pingouin as pg
 
 # Gnerate the data
 np.random.seed(123)
 x = np.arange(100)
-y = 30 + 0.4*x + 5*np.random.randn(len(x))
+y = 30 + 0.4 * x + 5 * np.random.randn(len(x))
 
 # Use a "DataFrame" to contain the data, and a "formula" to define the function
-df = pd.DataFrame({'x':x, 'y':y})
-formula = 'y~x'
+df = pd.DataFrame({"x": x, "y": y})
+
+# %% Solution with 'pingouin'
+for alpha in [0.05, 0.001]:
+    print(f" --------- Result with pingouin, alpha={alpha:.3f} ---------")
+    results = pg.linear_regression(df.x, df.y, alpha=alpha)
+    print(results)
+
+# %% Solution with 'statsmodels'
+print("\n --------------------- Results with statsmodels --------------------")
+formula = "y~x"
 results = smf.ols(formula, data=df).fit()
 
 # Show the results, with the default 95% confidence intervals
@@ -21,10 +30,10 @@ print(results.summary2())
 
 # Address some of the fit-parameters
 fit = {}
-fit['parameters'] = results.params
-fit['standard_error'] = results.bse
-fit['ci_95'] = results.conf_int()               # 95%   confidence intervals
-fit['ci_999'] = results.conf_int(alpha=0.001)   # 99.9% confidence intervals
+fit["parameters"] = results.params
+fit["standard_error"] = results.bse
+fit["ci_95"] = results.conf_int()  # 95%   confidence intervals
+fit["ci_999"] = results.conf_int(alpha=0.001)  # 99.9% confidence intervals
 
-for key in [key for key in fit.keys() if key.startswith('ci')]:
-    print(f'{key}: {fit[key]}')
+for key in [key for key in fit.keys() if key.startswith("ci")]:
+    print(f"{key}: {fit[key]}")
